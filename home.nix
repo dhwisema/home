1ddf819367096f08a9eba8df934495c7d1e0be1e
dhwisema <dhwisema@ncsu.edu>
2024-10-13 17:29:12 -0400

    i need to stop doing shit the annoying way

--- home.nix
+++ home.nix
@@ -6,6 +6,12 @@
   home.username = "howard";
   home.homeDirectory = "/home/howard";
 
+  nixpkgs.config = {
+  allowUnfree = true;
+  };
+
+
+
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
