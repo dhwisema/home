2dfce7e96401bf5d9d462e9290c6cb774b96c5e6
dhwisema <dhwisema@ncsu.edu>
2024-10-13 18:20:37 -0400

    bruh

--- flake.nix
+++ flake.nix
@@ -10,13 +10,12 @@
     };
     nixvim = {
         url = "github:nix-community/nixvim";
-        # If using a stable channel you can use `url = "github:nix-community/nixvim/nixos-<version>"`
         inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
 
-  outputs = { nixpkgs, home-manager, inputs, ... }:
+  outputs = inputs@{ nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
